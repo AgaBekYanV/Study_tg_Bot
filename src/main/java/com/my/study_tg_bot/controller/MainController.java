8ebc74fa2422e9404a1e7c95ec1a76306a4bc7af
AgaBekYanV <agabekyanv@yahoo.com>
2025-11-19 11:00:15 +0300

    Отзеркаливание сообщений пользователя

---
+++ src/main/java/com/my/study_tg_bot/controller/MainController.java
@@ -0,0 +1,53 @@
+package com.my.study_tg_bot.controller;
+
+import com.my.study_tg_bot.telegram.Bot;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.support.RouterFunctionMapping;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.w3c.dom.ls.LSOutput;
+
+@RestController
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class MainController {
+
+    final Bot bot;
+
+
+    @Autowired
+    public MainController(Bot bot) {
+        this.bot = bot;
+
+    }
+
+    @PostMapping("/")
+    public BotApiMethod<?> listener(@RequestBody Update update){
+        Message message = update.getMessage();
+        if(update.hasMessage()){
+            if(message.hasPhoto()){
+                return SendMessage.builder()
+                        .chatId(message.getChatId())
+                        .text("It's a photo")
+                        .build();
+            }
+            return echo(update.getMessage());
+        }
+        return bot.onWebhookUpdateReceived(update);
+    }
+
+    private BotApiMethod<?> echo(Message message) {
+        System.out.println(message.getText());
+        return SendMessage.builder()
+                .chatId(message.getChatId())
+                .text(message.getText())
+                .build();
+    }
+}
--- src/main/java/com/my/study_tg_bot/telegram/TelegramPropertys.java
+++
@@ -1,4 +0,0 @@
-package com.my.study_tg_bot.telegram;
-
-public class TelegramPropertys {
-}
