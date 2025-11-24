b3fa26c940d56126d47a873cebe80facc3bf61dc
AgaBekYanV <68870316+AgaBekYanV@users.noreply.github.com>
2025-11-24 14:29:08 +0300

    Grammatical corrections

--- src/main/java/com/my/study_tg_bot/service/manager/FeedbackManager.java
+++ src/main/java/com/my/study_tg_bot/service/manager/FeedbackManager.java
@@ -7,6 +7,7 @@ import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageTe
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+
 @Component
 public class FeedbackManager {
 
